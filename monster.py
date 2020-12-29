4c482f7bc06569576b15c94a5638a1e1d8ffcba2
mo-jinbuu <mohit-patel7@hotmail.com>
2020-12-28 19:57:06 -0500

    not working fml

--- monster.py
+++ monster.py
@@ -24,87 +24,19 @@ URL = "https://www.monster.ca/jobs/search/?q={}&where={}".format(jobURL, user_ci
 print('URL: ' + str(URL))
 root_url = 'https://www.monster.ca/'
 
-
-
-# Get total number of pagination (iteration count)
-getPage = requests.get(URL)
-getSoup = BeautifulSoup(getPage.content, 'html.parser')
-getCount = getSoup.find(id='searchCountPages')
-# Grab numbers from new jobs string
-paginationCount = re.findall(r'\b\d+\b', getCount.text)
-
-# FOR DEBUG
-print('---')
-print(paginationCount[1])
-print(math.ceil(int(paginationCount[1])/15))
-print('---')
-
-page_limit = math.ceil(int(paginationCount[1])/15)
-
 # Array of job listings
 jobListings = []
 
-# pageCount for url
-pageCount = 0
-page_route = '&start={}'.format(pageCount)
+# Request URL
+page = requests.get(URL)
+print('status code: ' + str(page.status_code))
 
-while(pageCount < page_limit*10):
+soup = BeautifulSoup(page.content, 'html.parser')
+getSearchResults = soup.find_all(class_="card-content")
+# print(getSearchResults)
 
-  # Add next page query parameter to URL to request new jobs
-  if (pageCount > 0):
-    myURL = URL + '&start={}'.format(pageCount)
-  else:
-    myURL = URL
+for cards in getSearchResults:
+  title = cards.find('a').getText()
+  print(title)
 
-  pageCount+=10
-
-  # Request URL
-  page = requests.get(myURL)
-
-  # print routes
-  print(myURL)
-  print('status code: ' + str(page.status_code))
-  print()
-
-  # If request fails, get out of loop
-  if page.status_code != 200:
-    print('Unable to process GET request for: ' + str(myURL))
-    break
-
-  soup = BeautifulSoup(page.content, 'html.parser')
-  results = soup.find(id='resultsCol')
-  resultsCards = soup.find_all(class_="jobsearch-SerpJobCard")
-
-  for cards in resultsCards:
-    title = cards.find('a', class_='jobtitle turnstileLink')
-    company = cards.find('div', class_='sjcl').find(class_='company')
-    location = cards.find('div', class_='sjcl').find(class_='location')
-    path = cards.find('a', class_='jobtitle turnstileLink').get('href')
-    full_path = root_url + path
-
-    #print(title.text.strip())
-    #print(company.text.strip())
-    #print(location.text.strip())
-    #print(full_path)
-
-    job = {
-      "title": title.text.strip(),
-      "company": company.text.strip(),
-      "location": location.text.strip(),
-      "job_URL": full_path
-    }
-
-    jobListings.append(job)
-
-
-count = 0
-## print all jobs
-for jobs in jobListings:
-  count+=1
-  print('Job#: ' + str(count))
-  print(jobs['title'])
-  print(jobs['company'])
-  print(jobs['location'])
-  print(jobs['job_URL'])
-  print()
-
+exit()
